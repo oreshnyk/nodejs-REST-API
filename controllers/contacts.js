7d53121e3fafbb837250ecf0fc5f420b525c89e9
oreshnyk <55344626+oreshnyk@users.noreply.github.com>
2023-05-15 20:59:55 +0200

    fixed errors

--- controllers/contacts.js
+++ controllers/contacts.js
@@ -1,48 +1,47 @@
-const contacts = require('../models/contacts');
-const { httpError, ctrlWrapper } = require('../utils');
-
+const contacts = require("../models/contacts");
+const { httpError, ctrlWrapper } = require("../utils");
 
 const listContacts = async (req, res) => {
-        const result = await contacts.listContacts();
-        res.json(result);
-        console.log(result);
+  const result = await contacts.listContacts();
+  res.json(result);
+  console.log(result);
 };
 
 const getContactById = async (req, res) => {
-        const { id } = req.params;
-        const result = await contacts.getContactById(id);
-        res.json(result);
+  const { contactId } = req.params;
+  const result = await contacts.getContactById(contactId);
+  res.json({ result });
 };
 
 const addContact = async (req, res) => {
-        const result = await contacts.addContact(req.body);
-        res.status(201).json(result);
+  const result = await contacts.addContact(req.body);
+  res.status(201).json(result);
 };
 
 const removeContact = async (req, res) => {
-        const { id } = req.params;
-        const result = await contacts.removeContact(id);
-        if (!result) {
-            throw httpError(404, 'Not found');
-        }
-        res.status(204).json({
-            message: 'Deleted'
-        });
+  const { contactId } = req.params;
+  const result = await contacts.removeContact(contactId);
+  if (!result) {
+    throw httpError(404, "Not found");
+  }
+  res.status(204).json({
+    message: "Deleted",
+  });
 };
 
 const updateContact = async (req, res) => {
-        const { id } = req.params;
-        const result = await contacts.updateContact(id, req.body);
-        if (!result) {
-            throw httpError(404, 'Not found');
-        }
-        res.json(result);
+  const { contactId } = req.params;
+  const result = await contacts.updateContact(contactId, req.body);
+  if (!result) {
+    throw httpError(404, "Not found");
+  }
+  res.json(result);
 };
 
 module.exports = {
-    listContacts: ctrlWrapper(listContacts),
-    getContactById: ctrlWrapper(getContactById),
-    addContact: ctrlWrapper(addContact),
-    removeContact: ctrlWrapper(removeContact),
-    updateContact: ctrlWrapper(updateContact),
-}
+  listContacts: ctrlWrapper(listContacts),
+  getContactById: ctrlWrapper(getContactById),
+  addContact: ctrlWrapper(addContact),
+  removeContact: ctrlWrapper(removeContact),
+  updateContact: ctrlWrapper(updateContact),
+};
