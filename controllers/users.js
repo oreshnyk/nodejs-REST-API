2f7d764a74aea5be16906eadda4a35f020babc9f
oreshnyk <55344626+oreshnyk@users.noreply.github.com>
2023-06-23 18:49:27 +0200

    hw8

--- controllers/users.js
+++ controllers/users.js
@@ -1,11 +1,20 @@
+const fs = require('fs/promises');
 const bcryptjs = require("bcryptjs");
 const jwt = require("jsonwebtoken");
+const gravatar = require("gravatar");
+const jimp = require("jimp");
+
 const { User } = require("../models/user");
-const { httpError, ctrlWrapper } = require("../utils");
+const { httpError, ctrlWrapper, cloudinary } = require("../utils");
 const { SECRET_KEY } = process.env;
 
 const register = async (req, res, next) => {
   const { email, password } = req.body;
+  // const { path: oldPath } = req.file;
+  // const fileData = await cloudinary.uploader.upload(oldPath, {
+  //   folder: "avatars"
+  // })
+  // await fs.unlink(oldPath);
 
   const user = await User.findOne({ email });
   if (user) {
@@ -15,30 +24,20 @@ const register = async (req, res, next) => {
     })
   }
 
-//   try {
-//     const hashedPassword = await bcryptjs.hash(password, 10);
-//     const newUser = await User.create({ ...req.body, password: hashedPassword });
-
-//     res.status(201).json({
-//       user: {
-//         email: newUser.email,
-//         subsription: newUser.subscription
-//       }
-//     });
-//   } catch (error) {
-//     next(error); // Pass the error to the error handling middleware
-//   }
-// };
-
   const hashedPassword = await bcryptjs.hash(password, 10);
+  // шаг 3
+  const avatarUrl = gravatar.url(email, { s: '200', r: 'pg', d: 'identicon' });
+  const normalizedAvatarUrl = avatarUrl.replace(/^\/\//, 'https://');
+  // const avatarUrl = fileData.url;
 
-  const newUser = await User.create({ ...req.body, password: hashedPassword });
+  const newUser = await User.create({ ...req.body, password: hashedPassword, avatarURL: normalizedAvatarUrl });
 
 
   res.status(201).json({
     user: {
       email: newUser.email,
-      subsription: newUser.subscription
+      subsription: newUser.subscription,
+      avatarURL: normalizedAvatarUrl
     }
   });
 };
@@ -54,32 +53,6 @@ const login = async (req, res, next) => {
     })
   }
 
-
-//   try {
-//     const comparePassword = await bcryptjs.compare(password, user.password);
-//     if (!comparePassword) {
-//       return res.status(401).json({
-//         message: "Email or password is wrong"
-//       });
-//     }
-
-//     const payload = {
-//       id: user._id,
-//     };
-//     const token = jwt.sign(payload, SECRET_KEY, { expiresIn: "23h" });
-//     await User.findByIdAndUpdate(user._id, { token });
-//     res.status(201).json({
-//       token,
-//       user: {
-//         email: user.email,
-//         subsription: user.subscription
-//       }
-//     });
-//   } catch (error) {
-//     next(error); // Pass the error to the error handling middleware
-//   }
-// };
-////////////////////////////////////////////////////////////////
   const comparePassword = await bcryptjs.compare(password, user.password);
   if (!comparePassword) {
     // throw httpError(401, "Email or password is wrong");
@@ -100,6 +73,45 @@ const login = async (req, res, next) => {
       subsription: user.subscription
     }
   });
+};
+
+const updateAvatar = async (req, res, next) => {
+  try {
+    const { path: tempFilePath, mimetype } = req.file;
+    const { _id } = req.user;
+
+    if (!tempFilePath) {
+      throw httpError(400, "No file uploaded");
+    }
+
+    const fileData = await cloudinary.uploader.upload(tempFilePath, {
+      folder: "avatars"
+    });
+
+    await fs.unlink(tempFilePath);
+
+    const image = await jimp.read(fileData.secure_url);
+    image.resize(250, 250).quality(80);
+
+    const processedAvatarPath = `tmp/${_id}_avatar.jpg`;
+    await image.writeAsync(processedAvatarPath);
+
+    const uniqueFilename = `${_id}_${Date.now()}${mimetype.replace("image/", ".")}`;
+    const avatarPath = `public/avatars/${uniqueFilename}`;
+    await fs.rename(processedAvatarPath, avatarPath);
+
+    const updatedUser = await User.findByIdAndUpdate(
+      _id,
+      { avatarURL: `/avatars/${uniqueFilename}` },
+      { new: true }
+    );
+
+    res.status(200).json({
+      avatarURL: updatedUser.avatarURL
+    });
+  } catch (error) {
+    next(error);
+  }
 };
 
 const getCurrentUser = async (req, res, next) => {
@@ -124,6 +136,7 @@ const logout = async (req, res, next) => {
 module.exports = {
   register: ctrlWrapper(register),
   login: ctrlWrapper(login),
+  updateAvatar: ctrlWrapper(updateAvatar),
   getCurrentUser: ctrlWrapper(getCurrentUser),
   logout: ctrlWrapper(logout),
 };
--- middlewars/upload.js
+++ middlewars/upload.js
@@ -5,6 +5,12 @@ const tempDir = path.join(__dirname, "../", "temp");
 
 const multerConfig = multer.diskStorage({
   destination: tempDir,
+  filename: (req, file, cb) => {
+    const uniquePreffix = Date.now() + "_" + Math.round(Math.random() * 1E9);
+    const {originalname} = file;
+    const filename = `${uniquePreffix}_${originalname}`;
+    cb(null, filename);
+  }
 });
 
 const upload = multer({
